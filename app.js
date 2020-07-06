a9c6de040ea0f286702954a4e2ca45a781972975
Petch <arisroung.sut@gmail.com>
2020-07-07 02:35:05 +0700

    updated app.js

--- app.js
+++ app.js
@@ -5,28 +5,30 @@ var path = require('path');
 
 app.use(express.static('public'));
 
+app.set ('view engine' , 'html');
+
 //app.get('/', function(req,res) {
 //    res.send('Hello World')
 //})
 
 app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname+'/public/หน้าหลัก.html'));
+    res.sendFile(path.join(__dirname+'/public/หน้าหลัก'));
 })
 
 app.get('/home', function(req,res) {
-    res.sendFile(path.join(__dirname+'/public/หน้าหลัก.html'));
+    res.sendFile(path.join(__dirname+'/public/หน้าหลัก'));
 })
 
 app.get('/gallery', (req,res) => {
-    res.sendFile(path.join(__dirname+'/public/แกลอรี่.html'));
+    res.sendFile(path.join(__dirname+'/public/แกลอรี่'));
 })
 
 app.get('/contact', (req,res) => {
-    res.sendFile(path.join(__dirname+'/public/ช่่องทางการติดต่อ.html'));
+    res.sendFile(path.join(__dirname+'/public/ช่่องทางการติดต่อ'));
 })
 
 app.get('/map', (req,res) => {
-    res.sendFile(path.join(__dirname+'/public/แผนที่.html'));
+    res.sendFile(path.join(__dirname+'/public/แผนที่'));
 })
 
 app.use(function(req, res, next) {
